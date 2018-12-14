858acaf96424ec0876fb654aed5d4499e4fe0d7c
Joshua <joshuacanadian@gmail.com>
2018-12-14 10:13:42 -0800

    added Auto_Function enum to robot_classes.h

--- include/robot_includes/robot_classes.h
+++ include/robot_includes/robot_classes.h
@@ -1,6 +1,14 @@
 #ifndef _ROBOT_CLASSES_H_
 #define _ROBOT_CLASSES_H_
 
+
+enum Auto_Function_Var {
+  COMPLETE,
+  INCOMPLETE,
+  FLAGGED
+};
+
+
 class Chassis {
 public:
   void setLeftPower(int power);
