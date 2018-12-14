10cf7b3f31eba48f6bbde61e49499df99b8fc19b
Joshua <joshuacanadian@gmail.com>
2018-12-14 10:09:57 -0800

    added Bal_System class to robot_classes.h

--- include/robot_includes/robot_classes.h
+++ include/robot_includes/robot_classes.h
@@ -3,8 +3,19 @@
 
 class Chassis {
 public:
-  void setLeftPower();
-  void setRightPower();
+  void setLeftPower(int power);
+  void setRightPower(int power);
+
+  void drive();
+};
+
+
+class Ball_System {
+public:
+  void setIntakePower(int power);
+
+  void setFlywheelPower(int power);
+  void setFlywheelRPM(int target);
 
   void drive();
 };
