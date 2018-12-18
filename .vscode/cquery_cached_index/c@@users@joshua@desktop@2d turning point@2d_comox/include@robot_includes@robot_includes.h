cf4f751f915ea0f5ace87789d059670ae4552a1a
Joshua <joshuacanadian@gmail.com>
2018-12-18 15:45:03 -0800

    changed code for catapult

---
+++ .vscode/cquery_cached_index/c@@users@joshua@desktop@2d turning point@2d_comox/include@robot_includes@robot_includes.h
@@ -0,0 +1,7 @@
+#ifndef _ROBOT_INCLUDES_H_
+#define _ROBOT_INCLUDES_H_
+
+#include "pid.h"
+#include "timing.h"
+
+#endif
---
+++ .vscode/cquery_cached_index/c@@users@joshua@desktop@2d turning point@2d_comox/include@robot_includes@timing.h
@@ -0,0 +1,8 @@
+#ifndef _TIMING_H_
+#define _TIMING_H_
+
+void resetTimer(int timer_slot);
+
+int getTime(int timer_slot);
+
+#endif
