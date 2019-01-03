94d320e812c13516b7fc43de4264d5669011b319
Joshua <joshuacanadian@gmail.com>
2019-01-02 22:18:54 -0800

    removed comments in opcontrol.cpp

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -1,18 +1,5 @@
 #include "main.h"
 
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
 	int i;
 	while (true) {
@@ -36,7 +23,7 @@ void opcontrol() {
 			cat = -127;
 		}
 		else if (cat_down.get_value() == true && master.get_digital(DIGITAL_R1)) {
-			cat = 0;
+			cat = -10;
 		}
 		else if (cat_down.get_value() == true && master.get_digital(DIGITAL_R2) == 0) {
 			cat = -127;
@@ -44,19 +31,6 @@ void opcontrol() {
 		else {
 			cat = 0;
 		}
-
-		/*
-		if (master.get_digital(DIGITAL_R1) && cat_down.get_value() == true) {
-			cat = -127;
-		}
-		else if (cat_down.get_value() == true) {
-			cat = 0;
-		}
-		else {
-			cat = -127;
-		}
-
-		*/
 
 		pros::delay(20);
 	}
